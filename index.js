64d0f50e3a2b852f6d591ec5b3cebef13d5d3730
Adnan Sahin <adnansahin1@outlook.com>
2024-01-04 16:33:07 +0300

    * Calculate the Wallet Balance * Calculate the balance before each Transaction * Wallet Balance from Recent Transaction * Wallet info request api is implemented

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/api/transact", (req, res) => {
     if (transaction) {
       transaction.update({ senderWallet: wallet, recipient, amount });
     } else {
-      transaction = wallet.createTransaction({ recipient, amount });
+      transaction = wallet.createTransaction({ recipient, amount, chain: blockchain.chain });
     }
   } catch (error) {
     return res.json({ type: "error", message: error.message });
@@ -67,6 +67,11 @@ app.get("/api/transaction-pool-map", (req, res) => {
 app.get("/api/mine-transactions", (req, res) => {
   tranasctionMiner.mineTransactions();
   res.redirect("/api/blocks");
+});
+
+app.get("/api/wallet-info", (req, res) => {
+  const address = wallet.publicKey;
+  res.json({ address, balance: Wallet.calculateBalance({ chain: blockchain.chain, address }) });
 });
 
 const syncWithRootState = () => {
